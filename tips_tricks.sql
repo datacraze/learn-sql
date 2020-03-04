28a32454ad38eb98c6b6fb3f83d1c04d93ddea24
Krzysztof Bury <krzysztof.bury@ge.comA>
2020-03-04 22:40:47 +0100

    Added SELECT to get table size for 1 script.

--- tips_tricks.sql
+++ tips_tricks.sql
@@ -16,7 +16,8 @@ INSERT INTO data_types_check_10 (product_name)
 select 'Product '||floor(random() * 10 + 1)::int
   from generate_series(1, 1000000) s(i);
 
--- SELECT pg_size_pretty( pg_database_size('dbname') );
+-- SELECT pg_size_pretty( pg_database_size('dbname'));
+-- SELECT pg_size_pretty( pg_table_size('schema.table'));
 
 CREATE INDEX product_name_text ON data_types_check_text USING btree(product_name);
 CREATE INDEX product_name_10 ON data_types_check_10 USING btree(product_name);
